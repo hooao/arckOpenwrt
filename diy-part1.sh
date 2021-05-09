74fbf5212996e0f14e58bd943705c592a130c50b
hooao <hooaosir@163.com>
2021-05-09 08:53:09 +0000

    Add some custom feeds and parameter.

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
+sed -i '$a src-git openwrtpackages https://github.com/openwrt/packages' feeds.conf.default
+sed -i '$a src-git spasswall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
